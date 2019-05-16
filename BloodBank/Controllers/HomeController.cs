0e7df9835dd7a3038edb54ea534b872d9d4e5380
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-16 22:46:10 +0300

    First Line Register For Blood Donors Added.

--- BloodBank/Controllers/HomeController.cs
+++ BloodBank/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace BloodBank.Controllers
             return View();
         }
 
+        public IActionResult FirstLineRegister()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
