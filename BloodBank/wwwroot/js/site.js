10cab5f0d3b0b5978d00ab13f1f2487aff936dc5
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-16 19:16:47 +0300

    Validation On Donation Page is Done.

--- BloodBank/wwwroot/js/site.js
+++ BloodBank/wwwroot/js/site.js
@@ -34,3 +34,41 @@ $('.search-bar').keyup(function () {
 $('.selection-bar').change(function () {
     retrieve();
 });
+
+var creditCardValidated, amountValidated;
+
+$('#NameOnCard').keyup(function () {
+    $(this).val($(this).val().toUpperCase());
+});
+
+$('#CreditCardNumber').keyup(function () {
+    var inputValue = $(this).val();
+    if (!$.isNumeric(inputValue) || inputValue.length != 16) {
+        $('#CreditCardValidation').text("Credit Card Number must be a 16 character number.");
+        creditCardValidated = false;
+    }
+    else {
+        $('#CreditCardValidation').text("");
+        creditCardValidated = true;
+    }
+});
+
+$('#Amount').keyup(function () {
+    var amountInput = $(this).val();
+
+    if (!$.isNumeric(amountInput)) {
+        $('#AmountValidation').text("Please enter a valid number.");
+        amountValidated = false;
+    }
+    else {
+        $('#AmountValidation').text("");
+        amountValidated = true;
+    }
+});
+
+$('#DonationForm').submit(function (e) {
+    if (!creditCardValidated || !amountValidated) {
+        $('#ValidationCheckText').text("Please check your inputs.");
+        e.preventDefault();
+    }
+});
